5640710861930f6488e5f8d09e6841814ead7cdc
GitHub Action <action@github.com>
2023-04-23 05:08:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/sukianata/testideaplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/sukianata/testideaplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.sukianata.testideaplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.sukianata.testideaplugin.MyBundle
+import com.github.sukianata.testideaplugin.services.MyProjectService
 import javax.swing.JButton
 
 
